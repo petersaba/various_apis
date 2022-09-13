a8c12436742c162de520bc60b30d233b0f4b0044
Peter Saba <petersaba001@gmail.com>
2022-09-13 09:51:54 +0300

    added comments

--- api1.php
+++ api1.php
@@ -1,11 +1,12 @@
 <?php 
     $input_name = $_GET['name'];
-
+    // compare the first character to the last character and moving to the next untill we reach the middle
     for ($i = 0; $i<strlen($input_name)/2; $i++){
         if ($input_name[$i] != $input_name[strlen($input_name)-1-$i]){
             $result = FALSE;
         }
     }
+    // result is only set if it is false so if it is not set then it should be true
     if(!isset($result)){
         $result = TRUE;
     }
